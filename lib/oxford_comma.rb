347521ddf0ff8e2d08edf51a882ce585400128bb
Dwight Nodolf <dnodolf@gmail.com>
2020-03-14 06:35:29 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+#a=["fruit","fruit","fruit","fruit"]
+#input --> array of string elements
+#output -->a string without formatting when given a 1 element array
+#         a string with and between the 2 elements 
+
+#         a strign with commas between them and "and" added to the last elements
+
+#         a string with many elements seperated by commas with an "and" added to the            last element
+
+
 def oxford_comma(array)
+  a = []
   array.join
+  
 end
