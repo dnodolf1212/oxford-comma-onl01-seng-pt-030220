bfa13e3450fdeb5efec0ca343a37932394ba7d85
Dwight Nodolf <dnodolf@gmail.com>
2020-03-14 03:50:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -13,7 +13,7 @@ def oxford_comma(array)
   elsif array.size == 2 
     array.join(" and ")
   elsif array.size == 3 
-  return array["0", "1"].join(", ") + ",and " + array[-1]
+  return array["0", "1"].join(", ") + (",and " + array[-1])
   
   end
 end
