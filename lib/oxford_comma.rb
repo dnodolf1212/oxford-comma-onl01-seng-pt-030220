07b911b758dd402867959e9472b9946fcd43af5d
Dwight Nodolf <dnodolf@gmail.com>
2020-03-14 04:20:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,8 @@ def oxford_comma(array)
      array.join(" and ")
 end
 
-def oxford_comma(array)
-  array.size == 3 
-  return array["0","1","-2"].join(" , ") + ", and" + array[-1]
+#def oxford_comma(array)
+  #array.size == 3 
+  #return array["0","1","-2"].join(" , ") + ", and" + array[-1]
   
-  end
+  #end
