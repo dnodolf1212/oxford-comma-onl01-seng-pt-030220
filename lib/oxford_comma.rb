1c4f9e5e411b612e99dea5b0e481d09df2ab4975
Dwight Nodolf <dnodolf@gmail.com>
2020-03-14 04:08:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -8,11 +8,7 @@ def oxford_comma(array)
 end
 
 def oxford_comma(array)
-  if array.size == 1 
-    return [1]
-  elsif array.size == 2 
-    array.join(" and ")
-  elsif array.size == 3 
+  array.size == 3 
   return array["0", "1"].join(", ") + (",and " + array[-1])
   
   end
