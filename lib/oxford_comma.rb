62c74d7ff84b1080a0e325532d15d3469bd6ba2d
Dwight Nodolf <dnodolf@gmail.com>
2020-03-14 06:12:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,3 @@
 def oxford_comma(array)
   array.join
 end
-
-def oxford_comma(array)
-     array.size == 2 
-     array.join(" and ")
-end
-
-def oxford_comma(array)
-  fruit = 0
-  array.size == 3 
-  array.join(" , ") 
-  
-  
-  
-  end
